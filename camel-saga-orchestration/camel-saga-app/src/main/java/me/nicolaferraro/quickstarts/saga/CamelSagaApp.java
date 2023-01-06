757cbc6abed18d3002f1feab0f370c20376716fd
shayashi1225 <33945075+shayashi1225@users.noreply.github.com>
2023-01-06 20:02:30 +0900

    Update CamelSagaApp.java

--- camel-saga-orchestration/camel-saga-app/src/main/java/me/nicolaferraro/quickstarts/saga/CamelSagaApp.java
+++ camel-saga-orchestration/camel-saga-app/src/main/java/me/nicolaferraro/quickstarts/saga/CamelSagaApp.java
@@ -22,8 +22,8 @@ public class CamelSagaApp {
             from("timer:clock?period=5s")
                     .saga()
                         .setHeader("id", header(Exchange.TIMER_COUNTER))
-                        .setHeader(Exchange.HTTP_METHOD, constant("POST"))
-                        .log("Executing saga #${header.id}")
+                        .setHeader(Exchange.HTTP_METHOD, condatrestant("POST"))
+                        .log("UpdateExecuting saga #${header.id}")
                         .to("http4://camel-saga-train-service:8080/api/train/buy/seat")
                         .to("http4://camel-saga-flight-service:8080/api/flight/buy");
 
